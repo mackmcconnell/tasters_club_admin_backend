4fd0a1c24b2d80769ad6b281a13dc2bc7c9d303b
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-24 17:15:16 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stripe::Application.config.secret_token = '15789793267689d30a6b39bc6486b49361b8a07bb16632a1ea00f853a171079c50a1a795278617a64c6e19cb050e973f8fed04414ad34a01a3e5bddbfb399931'
